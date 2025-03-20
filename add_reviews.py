d7e87e086dcc31f286221b9d4e8440e075940aa6
EmmaRichardson6148 <remma9443@gmail.com>
2025-03-19 20:34:04 -0400

    Update add_reviews.py
    added test reviews

--- add_reviews.py
+++ add_reviews.py
@@ -17,4 +17,9 @@ def add_review(product_id, user_id, rating, review_text):
 
   if __name__ == "__main__":
     print("\nAdding test reviews: \n")
-    add_review(1, 1, 5, 
+    add_review(1, 1, 5, "This is the perfect blouse for spring!")
+    add_review(1, 2, 4, "The material of this blouse isn't as soft as I expected.")
+    add_review(2, 3, 3, "I like the style of this shirt, but the fit is not what I wanted.")
+
+    print("Reviews added")
+
